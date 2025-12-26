b6bfa449c51df032f43ffa7261309a502d24ad54
lvflwr <60345253+lvflwr@users.noreply.github.com>
2025-12-27 05:02:55 +0900

    S8: refactor

--- spec/money/dollar_spec.rb
+++ spec/money/dollar_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe Dollar do
   describe "#times" do
     it "金額の掛け算ができること" do
       five = Money.dollar(5)
-      expect(five.times(2)).to eq(Dollar.new(10))
-      expect(five.times(3)).to eq(Dollar.new(15))
+      expect(five.times(2)).to eq(Money.dollar(10))
+      expect(five.times(3)).to eq(Money.dollar(15))
     end
   end
 
   describe "#==" do
     it "金額の等価性を判定できること" do
-      expect(Dollar.new(5) == Dollar.new(5)).to be true
-      expect(Dollar.new(5) == Dollar.new(6)).to be false
+      expect(Money.dollar(5) == Money.dollar(5)).to be true
+      expect(Money.dollar(5) == Money.dollar(6)).to be false
     end
   end
 end
