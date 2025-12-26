11880863bc4a8f88ab024429c85551758a8fc814
lvflwr <60345253+lvflwr@users.noreply.github.com>
2025-12-27 05:42:28 +0900

    S10: red

--- spec/money/money_spec.rb
+++ spec/money/money_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe Money do
     it "金額の等価性を判定できること" do
       expect(Money.franc(5) == Money.dollar(5)).to be false
     end
+
+    it "異なるクラス同士でも金額の等価性を判定できること" do
+      expect(Money.new(5, "CHF") == Franc.new(5, "CHF")).to be true
+    end
   end
 end
