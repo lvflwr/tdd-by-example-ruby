4d23f79a1e8bb5451582620e306419937d89778e
lvflwr <60345253+lvflwr@users.noreply.github.com>
2025-12-28 03:43:42 +0900

    S19: red

--- lib/xunit/test_case_test.rb
+++ lib/xunit/test_case_test.rb
@@ -9,6 +9,13 @@ class TestCaseTest < TestCase
     test.run
     raise "Test was not run after run method called" unless test.was_run == 1
   end
+
+  def test_setup
+    test = WasRun.new("test_method")
+    test.run
+    raise "Setup was not called before test method" unless test.was_setup
+  end
 end
 
 TestCaseTest.new("test_running").run
+TestCaseTest.new("test_setup").run
