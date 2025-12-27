d1c3cf382077341fd2ae4d8dc81c2ff563ef805b
lvflwr <60345253+lvflwr@users.noreply.github.com>
2025-12-28 03:51:21 +0900

    S19: refactor

--- lib/xunit/test_case_test.rb
+++ lib/xunit/test_case_test.rb
@@ -2,18 +2,18 @@ require_relative 'test_case'
 require_relative 'was_run'
 
 class TestCaseTest < TestCase
-  def test_running
-    test = WasRun.new("test_method")
-    raise "Test was run before run method called" unless test.was_run.nil?
+  def setup
+    @test = WasRun.new("test_method")
+  end
 
-    test.run
-    raise "Test was not run after run method called" unless test.was_run == 1
+  def test_running
+    @test.run
+    raise "Test was not run after run method called" unless @test.was_run == 1
   end
 
   def test_setup
-    test = WasRun.new("test_method")
-    test.run
-    raise "Setup was not called before test method" unless test.was_setup
+    @test.run
+    raise "Setup was not called before test method" unless @test.was_setup
   end
 end
 
--- lib/xunit/was_run.rb
+++ lib/xunit/was_run.rb
@@ -3,11 +3,6 @@ require_relative 'test_case'
 class WasRun < TestCase
   attr_reader :was_run, :was_setup
 
-  def initialize(name)
-    @was_run = nil
-    super(name)
-  end
-
   def setup
     @was_setup = 1
   end
