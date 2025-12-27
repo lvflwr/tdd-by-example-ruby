d7fcbc8234edae1ba66baa5b32efabbaba25c2ca
lvflwr <60345253+lvflwr@users.noreply.github.com>
2025-12-27 16:29:40 +0900

    S16: green

--- lib/money/sum.rb
+++ lib/money/sum.rb
@@ -10,4 +10,8 @@ class Sum
     amount = @augend.reduce(bank, to_currency).amount + @addend.reduce(bank, to_currency).amount
     Money.new(amount, to_currency)
   end
+
+  def plus(addend)
+    Sum.new(self, addend)
+  end
 end
