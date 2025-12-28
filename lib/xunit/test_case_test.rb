140870ffb210e1e7d3ca24dc0c3200f1da32cda6
lvflwr <60345253+lvflwr@users.noreply.github.com>
2025-12-29 07:46:09 +0900

    S23: red
    書籍ではテストの検証でPythonのassertを使用しているが、Rubyには
    assertが存在しないため、raiseで代用している。しかしそれにより、
    検証に失敗しても、TestCase#runのrescueに補足されて、テストが
    落ちない。本当はエラーハンドリングを改善して対処すべきだが、
    今回は写経が目的なので一旦テストが落ちると仮定して先に進む。

--- lib/xunit/test_case_test.rb
+++ lib/xunit/test_case_test.rb
@@ -26,9 +26,18 @@ class TestCaseTest < TestCase
     result.test_failed
     raise "Result summary does not match expected value" unless result.summary == "1 run, 1 failed"
   end
+
+  def test_suite
+    suite = TestSuite.new
+    suite.add(WasRun.new("test_method"))
+    suite.add(WasRun.new("test_broken_method"))
+    result = suite.run
+    raise "Result summary does not match expected value" unless result.summary == "2 run, 1 failed"
+  end
 end
 
 puts TestCaseTest.new("test_template_method").run.summary
 puts TestCaseTest.new("test_result").run.summary
 puts TestCaseTest.new("test_failed_result").run.summary
 puts TestCaseTest.new("test_failed_result_formatting").run.summary
+puts TestCaseTest.new("test_suite").run.summary
